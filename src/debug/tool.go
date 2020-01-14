496cbcaee7ecf5f8c8fb7bb1f5924eb17a7eb1ef
mattcrdb <mvardi@cockroachlabs.com>
2020-01-13 22:18:46 -0500

    prints non succeeded jobs

--- src/debug/tool.go
+++ src/debug/tool.go
@@ -60,6 +60,11 @@ func openZip(file string) error {
 						return err
 					}
 				}
+			case f.Name == "debug/crdb_internal.jobs.txt":
+				if err := getJobs(f); err != nil {
+					return err
+				}
+
 
 			default:
 
@@ -98,6 +103,56 @@ func readFromJson(f *zip.File) error {
 	}
 
 	//can also index into the map instead of for loop and checking if k == "<mykey>"
+
+	return nil
+}
+
+func getJobs(f *zip.File) error {
+	fmt.Println("NON SUCCEEDED JOBS:")
+	re := regexp.MustCompile(`succeeded`)
+
+	rff, err := f.Open()
+
+	if err != nil {
+		return err
+	}
+	defer rff.Close()
+
+	scanner := bufio.NewScanner(rff)
+
+	var gotCols bool = false
+	var gotDashes bool = false
+
+	for scanner.Scan() {
+		if !gotCols {
+			var cols = strings.Split(scanner.Text(),"|")
+			fmt.Println(cols[0] + "|" + cols[1] + "|" + cols[2] + "|" + cols[6] + "|" + cols[14] + "\n")
+			gotCols = true
+		} else if !gotDashes {
+			var dashes = strings.Split(scanner.Text(),"+")
+			fmt.Println(dashes[0] + "+" + dashes[1] + "+" + dashes[2] + "+" + dashes[3] + "+" + dashes[7] + "+" + dashes[15] + "\n")
+			gotDashes = true
+		} else {
+			if !re.MatchString(scanner.Text()) {
+				data := strings.Split(scanner.Text(), "|")
+				//Received a "panic: runtime error: index out of range" when reaching the last line of this file
+				//Workaround is checking the length of data, if it's 1 then EOF
+				if len(data) != 1 {
+					fmt.Println(data[0] + "|" + data[1] + "|" + data[2] + "|" + data[6] + "|" + data[14] + "\n")
+				}
+			}
+
+
+
+		}
+
+
+	}
+
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
 	return nil
 }
